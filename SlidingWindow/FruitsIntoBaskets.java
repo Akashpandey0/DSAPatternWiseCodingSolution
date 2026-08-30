9469a7947c6a85bd27d96465d8dce8e9e5065c53
Akash Pandey <akashpande027@gmail.com>
2026-08-30 20:20:18 +0530

    Fruits into baskets

---
+++ SlidingWindow/FruitsIntoBaskets.java
@@ -0,0 +1,60 @@
+// You are visiting a farm that has a single row of fruit trees arranged from left to right.
+// The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+// You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+// You only have two baskets, and each basket can only hold a single type of fruit. There is no limit on the amount of fruit each basket can hold.
+// Starting from any tree of your choice, you must pick exactly one fruit from every tree (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+// Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+// Given the integer array fruits, return the maximum number of fruits you can pick.
+
+// Example 1:
+
+// Input: fruits = [1,2,1]
+// Output: 3
+// Explanation: We can pick from all 3 trees.
+// Example 2:
+
+// Input: fruits = [0,1,2,2]
+// Output: 3
+// Explanation: We can pick from trees [1,2,2].
+// If we had started at the first tree, we would only pick from trees [0,1].
+// Example 3:
+
+// Input: fruits = [1,2,3,2,2]
+// Output: 4
+// Explanation: We can pick from trees [2,3,2,2].
+// If we had started at the first tree, we would only pick from trees [1,2].
+
+package SlidingWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FruitsIntoBaskets {
+    public static void main(String[] args) {
+        int [] fruits = {0,1,2,2};
+
+        System.out.println(maxFruits(fruits));
+    }
+
+    public static int maxFruits(int [] fruits) {
+        int atMost = 2;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int left = 0, right;
+        int maxLen = 0;
+
+        for(right = 0; right < fruits.length; right++) {
+            map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
+
+            while (map.size() > atMost) {
+                map.put(fruits[left], map.get(fruits[left]) - 1);
+                if(map.get(fruits[left]) == 0) map.remove(fruits[left]);
+                left++;
+            }
+            if(map.size() == atMost) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+        }
+        return maxLen;
+    }
+}
