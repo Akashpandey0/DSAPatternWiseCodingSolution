bb1fa8c5676ec172169d191d8ffd2c61ed1fdee4
Akash Pandey <akashpande027@gmail.com>
2026-09-05 10:35:14 +0530

    Max Subarray

---
+++ KadaneAlgorithmPattern/MaxSubArray.java
@@ -0,0 +1,47 @@
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+// Example 1:
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+// Example 2:
+// Input: nums = [1]
+// Output: 1
+// Explanation: The subarray [1] has the largest sum 1.
+
+// Example 3:
+// Input: nums = [5,4,-1,7,8]
+// Output: 23
+// Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+// Constraints:
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+
+package KadaneAlgorithmPattern;
+
+public class MaxSubArray {
+
+    public static void main(String[] args) {
+        int [] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int [] nums1 = {1};
+        int [] nums2 = {5,4,-1,7,8};
+
+        System.out.println(maxSubArray(nums));
+        System.out.println(maxSubArray(nums1));
+        System.out.println(maxSubArray(nums2));
+    }
+
+    public static int maxSubArray(int []nums) {
+        int currSum = nums[0];
+        int result =  nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            currSum = Math.max(nums[i], nums[i] + currSum);
+            result = Math.max(result, currSum);
+        }
+
+        return result;
+    }
+}
